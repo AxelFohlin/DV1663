1db4bfe94be654d6447a6334f214c9b1a6b2caaf
Axel Fohlin <axel.fohlin@gmail.com>
2024-05-16 16:35:04 +0200

    Co-authored-by: Asassing <Asassing@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, send_from_directory, redirect, url_for, session, flash
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for, session, flash, jsonify
 app = Flask(__name__, template_folder='website/template', static_folder='website/static')
 app.secret_key = 'pwd'
-from database import query_db, init_db
-from datetime import datetime
+from database import query_database, init_database
+import random
 
-init_db(app)
+init_database(app)
 
 ##################################### Multi-relation Query with #####################################
 # 1. At least two of the five queries should deal with data from more than one table, i.e., 
@@ -14,33 +14,18 @@ init_db(app)
 # 4. You should make use of at least two of the following:          
 #     a. Triggers                                                   [1/1]
 #     b. Procedures                                                 [1/1]
-#     c. Funcitons                                                  [0/1]
-
-##################################### Example future Queries #####################################
-
-# Trigger that checks if a bookings 
-# CREATE TRIGGER CheckMaxPlayersBeforeBooking
-# BEFORE INSERT ON bookings
-# WHEN (
-#     SELECT IFNULL(SUM(numberOfPlayers), 0)
-#     FROM bookings
-#     WHERE date = NEW.date AND tee_time = NEW.tee_time AND club_id = NEW.club_id
-# ) + NEW.numberOfPlayers > 4
-# BEGIN
-#     SELECT RAISE(ABORT, 'Total players for this tee time exceeds the maximum allowed.');
-# END;
+#     c. Funcitons                                                  [1/1]
 
 
-
-##################################### HELPER FUNCTIONS #####################################
+#--------------------------------------------------- Routes ---------------------------------------------------
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return send_from_directory('website/static', "home.html")
 
 @app.route("/players")
 def players():
-    return render_template("players.html", users=query_db("SELECT * FROM players"))
+    return render_template("players.html", users=query_database("SELECT * FROM players"))
 
 @app.route('/<path:filename>', methods=['GET'])
 def serve_static_html(filename):
@@ -59,14 +44,14 @@ def book():
         session["club_id"] = club_id
     golf_id = session.get('golf_id')
     date = request.args.get('booking_date')
-    # Example available times (adjust these according to your actual schedule)
+    club_name = query_database("SELECT club_name FROM courses WHERE club_id = ?", [club_id], one=True)
     available_times = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00']
     # Fetch previous bookings for the selected course and the logged-in user
     previous_bookings = []
     if date:
-        previous_bookings = query_db("SELECT bookings.booking_id, bookings.tee_time, bookings.date, bookings.numberOfPlayers FROM bookings where bookings.date = ? AND bookings.club_id = ?", [date, club_id])
+        previous_bookings = query_database("SELECT bookings.booking_id, bookings.tee_time, bookings.date, bookings.number_of_players FROM bookings where bookings.date = ? AND bookings.club_id = ? ORDER BY bookings.tee_time", [date, club_id])
         previous_bookings = [dict(row) for row in previous_bookings]
-    return render_template('book.html', club_id=club_id, golf_id=golf_id, available_times=available_times, previous_bookings=previous_bookings, date=date)
+    return render_template('book.html', club_id = club_id,club_name=club_name["club_name"], golf_id=golf_id, available_times=available_times, previous_bookings=previous_bookings, date=date)
 
 @app.route('/book', methods=['POST'])
 def book_create():
@@ -74,13 +59,14 @@ def book_create():
     golf_id = request.form['golf_id']
     session["club_id"] = club_id
     date = request.form['booking_date']
-    numberOfPlayers = request.form['NumberOfPlayers']
+    number_of_players = request.form['number_of_players']
     tee_time = request.form['booking_time']
 
     try:
-        query_db("INSERT INTO bookings(club_id, golf_id, numberOfPlayers, date, tee_time) VALUES(?, ?, ?, ?, ?)", [club_id, golf_id, numberOfPlayers, date, tee_time], commit=True)
-        calculate_greenfee(golf_id, club_id,numberOfPlayers)
+        query_database("INSERT INTO bookings(club_id, golf_id, number_of_players, date, tee_time) VALUES(?, ?, ?, ?, ?)", [club_id, golf_id, number_of_players, date, tee_time], commit=True)
+        calculate_greenfee(golf_id, club_id,number_of_players)
         session.pop("club_id", None)
+        session["booking_id"] = query_database("SELECT last_insert_rowid()", one=True)['last_insert_rowid()']
         return redirect(url_for('booking_confirmation'))
     except Exception as e:
         flash(str(e))
@@ -88,36 +74,39 @@ def book_create():
 
 @app.route('/booking_confirmation')
 def booking_confirmation():
-    return render_template('booking_confirmation.html')
+    booking_id = session.get("booking_id")
+    booking_information = booking_confirmation_information(booking_id)
+    session.pop("booking_id")
+    return render_template('booking_confirmation.html', booking_details=booking_information)
 
 
 @app.route('/profile')
 def profile():
-    golf_id = session.get('golf_id')  # Assuming you store the logged-in user's golf_id in the session
-    club_id = request.args.get('club_id')  # Get club_id from the query parameters for filtering
+    golf_id = session.get('golf_id')
+    club_id = request.args.get('club_id')
 
     # Fetch user details
-    user_details = query_db("SELECT players.first_name, players.last_name, players.golf_id, players.email, players.hcp, courses.club_name FROM players JOIN membership ON players.golf_id = membership.golf_id JOIN courses ON membership.club_id = courses.club_id WHERE players.golf_id = ?", [golf_id], one=True)
+    user_details = query_database("SELECT players.first_name, players.last_name, players.golf_id, players.email, players.hcp, courses.club_name FROM players JOIN membership ON players.golf_id = membership.golf_id JOIN courses ON membership.club_id = courses.club_id WHERE players.golf_id = ?", [golf_id], one=True)
 
     # Fetch courses for dropdown
-    courses = query_db("SELECT club_id, club_name FROM courses")
+    courses = query_database("SELECT club_id, club_name FROM courses")
 
     # Build the query for booking history
     query = """
-        SELECT b.booking_id, b.club_id, b.date, b.tee_time, b.numberOfPlayers, b.price, c.club_name
-        FROM bookings b
-        JOIN courses c ON b.club_id = c.club_id
-        WHERE b.golf_id = ?
+        SELECT bookings.booking_id, bookings.club_id, bookings.date, bookings.tee_time, bookings.number_of_players, bookings.price, courses.club_name
+        FROM bookings
+        JOIN courses ON bookings.club_id = courses.club_id
+        WHERE bookings.golf_id = ?
     """
     params = [golf_id]
 
     # If a club_id is provided, filter by it
     if club_id:
-        query += " AND b.club_id = ?"
+        query += " AND bookings.club_id = ?"
         params.append(club_id)
 
-    query += " ORDER BY b.date DESC, b.tee_time ASC"
-    booking_history = query_db(query, params)
+    query += " ORDER BY bookings.date DESC, bookings.tee_time ASC"
+    booking_history = query_database(query, params)
 
     return render_template('profile.html', user=user_details, bookings=booking_history, courses=courses)
 
@@ -127,7 +116,7 @@ def login():
     password = request.form['password']
     print("golf id: ", golf_id)
     print("password: ", password)
-    player = query_db("SELECT * FROM players WHERE golf_id = ? and password = ?", [golf_id, password],one=True)
+    player = query_database("SELECT * FROM players WHERE golf_id = ? and password = ?", [golf_id, password],one=True)
     print("player: ", player)
     if player:
         print("adding golf_id to session")
@@ -135,6 +124,19 @@ def login():
         return redirect(url_for('profile', golf_id=golf_id))
     return redirect(url_for('login', golf_id=golf_id))
 
+@app.route('/calculate_price', methods=['POST'])
+def calculate_price():
+    golf_id = request.form['golf_id']
+    club_id = request.form['club_id']
+    number_of_players = int(request.form['number_of_players'])
+    
+    green_fee = query_database("SELECT greenfee FROM courses WHERE club_id = ?", [club_id], one=True)['greenfee']
+    
+    # Check membership
+    is_member = query_database("SELECT 1 FROM membership WHERE golf_id = ? AND club_id = ?", [golf_id, club_id], one=True)
+    price = 0 if is_member else green_fee * number_of_players
+    
+    return jsonify({'price': price})
 
 
 @app.route('/courses', methods=['GET'])
@@ -142,21 +144,32 @@ def show_courses():
     if session.get("club_id"):
         session.pop("club_id")
     golf_id = session.get('golf_id')
-    user = query_db('SELECT * FROM Players WHERE golf_id = ?', [golf_id], one=True)
-    courses = query_db("SELECT * FROM Courses")
+    user = query_database('SELECT * FROM Players WHERE golf_id = ?', [golf_id], one=True)
+    courses = query_database("SELECT * FROM Courses")
     return render_template('courses.html', courses=courses, user=user)
 
 @app.route('/register/new_user', methods=['POST'])
 def register():
-    
     birthdate_str = request.form['birthdate']
     golf_id = generate_golf_id(birthdate_str)
     password = request.form['password']
     email = request.form['email']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    query_db("INSERT INTO players(golf_id, password, email, first_name, last_name) VALUES(?, ?, ?, ?, ?)", [golf_id, password, email, first_name, last_name], commit=True)
-    return send_from_directory('website/static', 'login.html')
+    hcp = random_hcp()
+    query_database("INSERT INTO players(golf_id, password, email, first_name, last_name, hcp) VALUES(?, ?, ?, ?, ?, ?)", [golf_id, password, email, first_name, last_name, hcp], commit=True)
+    # return send_from_directory('website/static', 'login.html')
+    print("here")
+    return redirect(url_for('registration_success', golf_id=golf_id))
+
+@app.route('/register')
+def register_courses():
+    courses = query_database("SELECT * FROM courses")
+    return render_template("register.html", courses=courses)
+
+@app.route('/register/success/<golf_id>')
+def registration_success(golf_id):
+    return render_template('register_confirmation.html', golf_id=golf_id)
 
 @app.route('/logout')
 def logout():
@@ -165,15 +178,16 @@ def logout():
 
 
 #--------------------------------------------------- HELPER FUNCTIONS ---------------------------------------------------
-#Generate golf_id
+
 def generate_golf_id(birthdate_str):
+    # Generate golf_id
     # Extract year, month, and day from birthdate string
-    birth_year = birthdate_str[:2]  # Extract first two characters for the year
-    birth_month = birthdate_str[2:4]  # Extract characters 3 and 4 for the month
-    birth_day = birthdate_str[4:]  # Extract the last two characters for the day
+    birth_year = birthdate_str[:2]
+    birth_month = birthdate_str[2:4]
+    birth_day = birthdate_str[4:]
 
     # Count the number of users with the same birthdate
-    count = query_db("SELECT COUNT(*) FROM players WHERE golf_id LIKE ?", 
+    count = query_database("SELECT COUNT(*) FROM players WHERE golf_id LIKE ?", 
                      (f"{birth_year}{birth_month}{birth_day}-%",), one=True)
 
     # Generate golf ID with suffix based on the count
@@ -182,19 +196,41 @@ def generate_golf_id(birthdate_str):
 
     return golf_id
 
+def random_hcp():
+    return random.randint(0, 54)
+
+#--------------------------------------------------- Stored Procedure ---------------------------------------------------
 def calculate_greenfee(golf_id, club_id, number_of_players):
     # Stored Procedure
-    membership = query_db("SELECT 1 FROM membership WHERE golf_id = ? AND club_id = ?", [golf_id, club_id], one=True)
-    booking_id = query_db("SELECT last_insert_rowid()", one=True)['last_insert_rowid()']
+    membership = query_database("SELECT 1 FROM membership WHERE golf_id = ? AND club_id = ?", [golf_id, club_id], one=True)
+    booking_id = query_database("SELECT last_insert_rowid()", one=True)['last_insert_rowid()']
     if membership:
         price = 0
     else:
-        greenfee = query_db("SELECT greenfee FROM courses WHERE club_id = ?", [club_id],one=True)["greenfee"]
+        greenfee = query_database("SELECT greenfee FROM courses WHERE club_id = ?", [club_id],one=True)["greenfee"]
         print(float(greenfee))
         print(float(number_of_players))
         price = float(greenfee) * float(number_of_players)
-    query_db("UPDATE bookings SET price = ? WHERE booking_id = ?", [price, booking_id], commit=True)
-    
+    query_database("UPDATE bookings SET price = ? WHERE booking_id = ?", [price, booking_id], commit=True)
+
+#--------------------------------------------------- Function ---------------------------------------------------
+def booking_confirmation_information(booking_id):
+    # Function
+    return query_database("SELECT bookings.booking_id, bookings.date as booking_date, bookings.tee_time, bookings.number_of_players, bookings.price as Price_per_person, courses.club_name,players.first_name, players.last_name, players.email FROM bookings JOIN courses ON bookings.club_id = courses.club_id JOIN players ON bookings.golf_id = players.golf_id WHERE bookings.booking_id = ?", [booking_id], one=True)
+
+
+#--------------------------------------------------- Trigger ---------------------------------------------------
+# CREATE TRIGGER CheckMaxPlayersBeforeBooking
+# BEFORE INSERT ON bookings
+# WHEN (
+#     SELECT IFNULL(SUM(number_of_players), 0)
+#     FROM bookings
+#     WHERE date = NEW.date AND tee_time = NEW.tee_time AND club_id = NEW.club_id
+# ) + NEW.number_of_players > 4
+# BEGIN
+#     SELECT RAISE(ABORT, 'Total players for this tee time exceeds the maximum allowed.');
+# END;
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -4,35 +4,31 @@ from flask import g
 
 DATABASE = 'database.db'
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-    return db
+def get_database():
+    database = getattr(g, '_database', None)
+    if database is None:
+        database = g._database = sqlite3.connect(DATABASE)
+        database.row_factory = sqlite3.Row
+    return database
 
-def close_db(exception=None):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+def close_database(exception=None):
+    database = getattr(g, '_database', None)
+    if database is not None:
+        database.close()
 
 
-
-# ---------------- QUERYING FUNCTIONS --------------
-def query_db(query, args=(), one=False, commit=False):
+def query_database(query, args=(), one=False, commit=False):
     if commit:
         print("commit")
-        db = get_db()
-        cur = db.execute(query, args)
-        db.commit()
+        database = get_database()
+        cur = database.execute(query, args)
+        database.commit()
         cur.close
         return None
-    cur = get_db().execute(query, args)
+    cur = get_database().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-
-# When teardown context we close the database.
-def init_db(app):
-    app.teardown_appcontext(close_db)
+def init_database(app):
+    app.teardown_appcontext(close_database)
